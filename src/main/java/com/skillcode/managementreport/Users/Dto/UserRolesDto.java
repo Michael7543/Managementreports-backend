d17ddbac7824b73efc3a0823e31afd9e8686e4e5
Michael7543 <mdr.moran@yavirac.edu.ec>
2025-07-25 14:36:46 -0500

    [Feat](Dto): aplicar validaciones (@NotBlank, @NotNull, @Size, etc.) en los DTOs y ajustar tipos de datos

---
+++ src/main/java/com/skillcode/managementreport/Users/Dto/UserRolesDto.java
@@ -0,0 +1,23 @@
+package com.skillcode.managementreport.Users.Dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRolesDto {
+    private long id;
+    @NotNull(message = "El ID de usuario no puede ser nulo")
+    @Min(value = 1, message = "El ID de usuario debe ser mayor a 0")
+    private Long userId;
+
+    @NotNull(message = "El ID de rol no puede ser nulo")
+    @Min(value = 1, message = "El ID de rol debe ser mayor a 0")
+    private Long rolId;
+}
