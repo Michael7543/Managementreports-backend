99d14de28712f6b698fb00ad5de4803a6f605d29
Michael7543 <mdr.moran@yavirac.edu.ec>
2025-09-20 16:25:51 -0500

    [fix](dto): se cambio el nombre de la relacion de GeneralInfoDto y DetailInfoDTO

--- src/main/java/com/skillcode/managementreport/Users/infrastructure/rest/dto/DetailInfoDto.java
+++ src/main/java/com/skillcode/managementreport/Users/infrastructure/rest/dto/DetailInfoDto.java
@@ -1,51 +1,26 @@
 package com.skillcode.managementreport.Users.infrastructure.rest.dto;
 
-import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class DetailInfoDto {
-    @NotBlank(message = "El detalle de función no puede estar vacío")
-    @Size(max = 100, message = "El detalle de función no debe superar los 100 caracteres")
-    private String functionDetail;
+@Builder
+public class DetailInfoDTO {
+    private UUID id;
+    private String observations;
 
-    @NotBlank(message = "La actividad no puede estar vacía")
-    private String activity;
-
-    @NotBlank(message = "La subactividad no puede estar vacía")
-    private String subActivity;
-
-    @NotBlank(message = "La estrategia no puede estar vacía")
-    private String stategy;
-
-    @NotBlank(message = "El tipo de lugar no puede estar vacío")
-    private String typePlace;
-
-    @NotBlank(message = "La zona administrativa no puede estar vacía")
-    private String administrationZona;
-
-    @NotBlank(message = "El espacio público no puede estar vacío")
-    private String publicSpace;
-
-    @NotNull(message = "La latitud no puede ser nula")
-    @DecimalMin(value = "-90.0", message = "La latitud no puede ser menor a -90")
-    @DecimalMax(value = "90.0", message = "La latitud no puede ser mayor a 90")
-    private Double latitude;
-
-    @NotNull(message = "La longitud no puede ser nula")
-    @DecimalMin(value = "-180.0", message = "La longitud no puede ser menor a -180")
-    @DecimalMax(value = "180.0", message = "La longitud no puede ser mayor a 180")
-    private Double longitude;
-
-    @NotBlank(message = "La dirección no puede estar vacía")
-    private String direction;
-
-    @NotNull(message = "El ID de usuario no puede ser nulo")
-    private Long userId;
+    private ActivitiesDetailDTO activitiesDetail;
+    private SubActivitiesDetailDTO subActivitiesDetail;
+    private LocationDetailDTO locationDetail;
+    private GeneralInfoDto generalInfo;
+    private TypeSpaceDetailDTO typeSpaceDetail;
+    private AdministrationZoneDetailDTO administrationZoneDetail;
+    private PublicSpaceDetailDTO publicSpaceDetail;
+    private FunctionDetailDTO functionDetail;
 }
